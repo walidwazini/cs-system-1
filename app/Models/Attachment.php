f18637ecc8e7a741effbb011164f048b2ba055ec
Walid Wazini <wazini25@gmail.com>
2023-10-10 09:05:22 +0800

    new function in Attachment model

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use Storage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Attachment extends Model
-{
+class Attachment extends Model {
     use HasFactory;
+
+    public $table = 'attachments';
+
+    protected $fillable = ['parent_model', 'parent_id', 'path'];
+
+    public static function put($files, $parent_id, $parent_model) {
+        $attachments = [];
+
+        foreach ($files as $key => $file) {
+            $filename = rand() . '-' . $file->getClientOriginalName();
+
+            $attachments[] = [
+                'parent_id' => $parent_id,
+                'parent_model' => $parent_model,
+                'path' => $filename
+            ];
+
+            // ?  Update our storage in  app
+            Storage::put("public/$filename", file_get_contents($file->getRealPath()));
+        }
+
+        // ?   Record the transaction in the database
+        Attachment::insert($attachments);
+        return $attachments;
+    }
 }
